2ba805e4b1522d351fd424dee092337f1563b286
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-10-26 08:13:18 +0100

    Added fetch of data.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import './Sidebar.css';
-import jsondata from './types.json';
 
 class Sidebar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('/api/types')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    })
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: false,
+                        error: error
+                    })
+                }
+            )
+    }
 
     render() {
-        let types = [];
+        let typelist = undefined;
 
-        types = this.props.types;
-
-        let typelist = types.map((type) => {
-            let href = `/api/types/${type.sensor_type_cd}/sensors/?nh_sens_id=null`
-            return (
-            <a href={href} className="_list-item" key={type.sensor_type_cd}>
-                <span>{type.sensor_type_cd}  {type.sensor_type_sdesc}</span>
-            </a>
-            )
-        })
+        if (this.state.items.length === 0) {
+            typelist = <p>Failed to load Sensor Types with error {this.state.error}</p>
+        }
+        else {
+            typelist = this.state.items.map((type) => {
+                let href = `/api/types/${type.sensor_type_cd}/sensors/?nh_sens_id=null`
+                return (
+                <a href={href} className="_list-item" key={type.sensor_type_cd}>
+                    <span>{type.sensor_type_cd}  {type.sensor_type_sdesc}</span>
+                </a>
+                )
+            })
+        }
 
         return (
             <div className="_list" role="navigation">
