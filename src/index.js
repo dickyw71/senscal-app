860213149f8d313d18b7cb4e892f6f5f6dae4dc2
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-10-26 08:13:36 +0100

    Removed fetch of data.

--- src/index.js
+++ src/index.js
@@ -4,27 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-async function getTypes() {
-    try {
-        let response = await fetch('/api/types');
 
-        let types = await response.json();
-        
-        console.log(types.length)
 
-        ReactDOM.render(<App types={types} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-        // If you want your app to work offline and load faster, you can change
-        // unregister() to register() below. Note this comes with some pitfalls.
-        // Learn more about service workers: http://bit.ly/CRA-PWA
-        serviceWorker.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
-    }
-    catch (e) {
-        console.error(e.message)
-    }
-}
-
-getTypes();
 
 
