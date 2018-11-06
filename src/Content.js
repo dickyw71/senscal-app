adf2d558b01ba1456eb2bd97b8713dcb31dabc9b
Richard Wheatley <dickyw71@users.noreply.github.com>
2018-11-06 21:54:58 +0000

    Initial commit.

---
+++ src/Content.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Content.css';
+
+
+class Content extends Component {
+    constructor(props) {
+        super(props)
+
+    }
+
+    render() {
+        return (
+            <div className="_intro">
+                <h1>Sensor Calibration</h1>   
+            </div>
+        )
+    }
+}
+
+export default Content;
