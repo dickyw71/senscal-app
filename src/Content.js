c662382038d8186d3c94036ad583d4ef5ff6045b
Richard Wheatley <dickyw71@users.noreply.github.com>
2019-12-27 00:09:37 +0000

    Added a p element  with a description of sensor statistics.

--- src/Content.js
+++ src/Content.js
@@ -9,10 +9,12 @@ class Content extends Component {
     }
 
     render() {
+        const { typesCount, sensorsCount, partsCount, calibrationsCount} = this.props.stats
         return (
             <div className="_intro">
                 <h1>Sensor Calibration</h1>   
                 <p>{this.props.uri}</p>
+                <p>Currently there are {typesCount} sensor types comprised of {sensorsCount} sensors, based on {partsCount} parts and with {calibrationsCount} calibration records.</p>
             </div>
         )
     }
