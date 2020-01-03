2af65212f6b252ae8e2af85d221d45e1669eebce
dickyw71 <dickyw71@users.noreply.github.com>
2020-01-03 21:15:06 +0000

    Added itemClicked method to retrive and locally store the calibration for use as content.

--- src/CalibrationItem.js
+++ src/CalibrationItem.js
@@ -4,14 +4,34 @@ import './List-Item.css';
 class CalibrationItem extends Component {
     constructor(props) {
         super(props)
+
+        this.itemClicked = this.itemClicked.bind(this);
+    }
+
+    itemClicked() {
+        const calibrationUri = `/api/calibrations/${this.props.item.sensor_cal_id}?view=full`
+        fetch(calibrationUri)
+        .then( resp => resp.json())
+        .then(
+            (result) => {
+                localStorage.setItem(calibrationUri, JSON.stringify(result))
+                this.props.updateContentUri(calibrationUri)
+            },
+            (error) => {}
+        )    
     }
 
     render() {
         let cal_date = new Date(Date.parse(this.props.date)).toDateString()
         return (
             <a
+                href={`/api/calibrations/${this.props.item.sensor_cal_id}`}
                 className="_list-item _list-dir"
                 key={this.props.item.cert_no}
+                onClick={(e) => { 
+                    e.preventDefault() 
+                    this.itemClicked()
+                }}
                 tabIndex="-1"
             >
                 <span
